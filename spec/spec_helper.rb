a74558d64a0eb17efb99da48422bd4b773ca1d3d
Paul Borg <paul.borg@myob.com>
2016-08-30 12:04:31 +1000

    Update Coverage Reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,5 @@
-require 'rspec'
 require 'simplecov'
-require "codeclimate-test-reporter"
+SimpleCov.start
 
-#SimpleCov.start
-CodeClimate::TestReporter.start
-
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-  config.order = :random
-  config.formatter = :documentation
-end
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
